66d47068f42a48c22dded09763b7417302af55b4
Mayank Raut <raut.mayank04@gmail.com>
2026-05-29 12:52:08 +0000

    Learn variables basic

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 44542
+// accountId = 56565  this is not allowed 
+let getEmail = "mayank@gmail.com"
+getEmail="Anuj@gmail"
+var Password = "68565"
+Password="bhendi56kg"
+//Prefer not to use Var because of  issue in block schope and functionctional scope
+AccountCity="bhindibajar"
+AccountCity="ghatkopar"
+
+// console.log(accountId) For variable call
+
+console.table([accountId,getEmail,Password,AccountCity])
+
