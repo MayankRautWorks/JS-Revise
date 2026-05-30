c6948453f00b88a25bde7b8a86aea930173882ff
Mayank Raut <raut.mayank04@gmail.com>
2026-05-30 08:20:03 +0000

    Datatypes Que solving practice

---
+++ 01_Basic/02_Datatypes.js
@@ -0,0 +1,71 @@
+  //There are 7 types of Datatypes in js
+
+  // String => ""
+  // Number => 43
+  // Boolean => True/false
+  // null => This is stand Alone Value Stands for empty value // And this is Type off will be Object
+  // undefind => Undefined
+  // Bigint =>28579531513553n
+  // Symbol => Unique
+
+
+//   let a  = 10 ;
+//   console.log(typeof a)   OutPut will => Number
+
+
+// let b = "mayank"
+// console.log(typeof b);  Output will be  => String "mayank"
+
+// let isLoginIn ="true"
+// console.log(typeof isLoginIn);   => True
+
+
+// let a 
+// console.log(typeof a)  OutPut => undefined
+
+// let a = null;
+// console.log(typeof a);  Type off => Object
+
+
+// let arr = [1, 2, 3];       
+// console.log(typeof arr);  Output will => Object
+
+// let obj = {
+//   name: "Mayank"
+// };
+// console.log(typeof obj);   Output will be  => Object
+
+// let a = 10n;
+// console.log(typeof a);    Output Will be => bihint
+
+// let id = Symbol("id");     Output will be symbol => Symbol
+// console.log(typeof id);
+
+// console.log(typeof NaN);  Out put => Number
+
+
+//  console.log(typeof function() {});  OutPut => function
+
+// null    => null type of   Object 
+// undefined => undefined type off undefined 
+
+// console.log(Array.isArray([1, 2, 3]));   Output will be => true 
+
+// console.log([] == false);  => OutPut => true
+
+// console.log("5" + 2);  OutPut will be => 52
+
+// console.log("5" - 2);  OutPut will be => 3
+
+// console.log(true + true);   OutPut will be => 2
+
+// console.log(null == undefined);  Out put => true 
+// console.log(null === undefined);  output => false
+
+// console.log(typeof typeof 100);  output => string
+
+// console.log([] + []);  Output will be => Blank 
+ 
+// console.log([] + {});  output will be => [object Object]
+
+// console.log({} + []);   output will be  => [object object]
